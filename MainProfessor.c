bf56d7ccbe27cf6b5c51a61432e23ab19e922181
KassianoV <kassianovieira@id.uff.br>
2022-01-24 16:01:45 -0300

    MainProfessor Atualizada

--- MainProfessor.c
+++ MainProfessor.c
@@ -9,8 +9,8 @@ typedef struct professor{
     double salario;
 }*Professor;
 
-Professor CriaProfessor(){
-    Professor pf=(Aluno) malloc(sizeof(struct professor));
+Professor *CriaProfessor(){
+    Professor pf=(Professor) malloc(sizeof(struct professor));
     printf("\nInforme a matricula do professor: ");
     scanf("%d",&pf->matricula);
     getchar();
@@ -27,29 +27,52 @@ Professor CriaProfessor(){
     return pf;
 }
 
+int menu(){
+    int opc;
+    printf("\nEscolha uma opcao:\n");
+    printf("[1] Adicionar um aluno\n");
+    printf("[2] Imprimir informacoes de um Aluno\n");
+    printf("[3] Imprimir Tabela Hash\n");
+    printf("[4] Encerrar\n");
+    scanf("%d",&opc);
+    return opc;
+}
+
+Professor *InicializaAluno(Professor pf){
+    pf=(Professor) malloc(sizeof(Professor));
+}
+
 int main(){
     int mat;
-    int opcao=0;
+    int opcao=0,aux;
     char nomeArq[13];
     strcpy(nomeArq,"registro.bin");
-    Aluno pf;
-    //Professor p = (Professor) malloc (sizeof(struct professor));
+    Professor pf=InicializaAluno(pf);
+    InicializaArq(nomeArq, pf,sizeof(Professor));
     
     while(opcao != 4){
         opcao=menu();
         switch (opcao){
             case 1:
-                pf=CriaAluno();
-                Inserir(nomeArq, 0, pf, sizeof(Aluno));
+                pf=CriaProfessor();
+                Inserir(nomeArq, 0, pf, sizeof(Professor));
                 break;
             case 2:
-                printf("\nDigite o numero da matricula do aluno: ");
+                printf("\nDigite o numero da matricula do professor: ");
                 scanf("%d",&mat);
-                pf=(Aluno) BuscaElemento(nomeArq,mat);
+                if(BuscarObj(nomeArq,mat,pf,sizeof(struct professor))==1){
+                    printf("Os dados do professor sao:\n");
+                    printf("Matricula: %d\nNome: %s\nEmail: %d",pf->matricula, pf->nome, pf->salario);
+                }else
+                    printf("Nao ha um aluno com matricula %d no registro",mat);
                 break;
             case 3:
+                printf("\n");
+                leituraCompleta(nomeArq);
+                break;
                 break;
             case 4:
+                printf("Saindo...");
                 break;
             default:
                 printf("Digite uma opcao valida!\n");
--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Seminário de implementação 
-### Tema:TAD Hashing Genérico
+<h1> Seminário de implementação </h1>
+<h2> Tema:TAD Hashing Genérico </h2>
 
-#### Grupo: Gabriel Silva, João Henrique e Kassiano Vieira
-
-
-
-#### Compilando pelo gcc
+<h4> Grupo: Gabriel Silva, João Henrique e Kassiano Vieira</h4>
+<br/>
+<h4>Compilando pelo gcc:</h4>
 
 ~~~ bash
 gcc -c HashingGen.c
@@ -18,7 +16,6 @@ gcc -o MainAluno.exe HashingGen.o MainAluno.o
 * ##### gcc -o MainAluno.exe HashingGen.o MainAluno.o: Criando executável
 </br>
 
-
-
-##### Pode trocar _MainAluno.c_ e _MainAluno.o_ por _MainProfessor.c_ e _MainProfessor.o_ dependendo do que você deseja compilar
-##### Se tiver no Linux Troque o _MainAluno.exe_ => _MainAluno_
+<h4>Substituição de Objeto que deseja utilizar</h4>
+<p> Pode trocar  <strong>MainAluno.c</strong> e <strong>MainAluno.o</strong>  por <strong>MainProfessor.c</strong> e <strong>MainProfessor.o</strong> dependendo do que você deseja compilar
+Se tiver no Linux Troque o <strong>MainAluno.exe_ => _MainAluno_</strong>
